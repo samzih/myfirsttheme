84f68b11bf7e766d133df42bd607a0c0d9d0ce20
samzih <samzih1337@gmail.com>
2023-04-11 21:49:58 +0200

    theme support added

--- functions.php
+++ functions.php
@@ -1,5 +1,13 @@
 <?php
 
+// support for theme
+function theme_support(){
+    add_theme_support('menus');
+    add_theme_support('post-thumbnails');
+}
+add_action('after_setup_theme', 'theme_support');
+
+
 // loads the misc css styling files
 function load_css(){
     wp_enqueue_style('bootstrap', get_template_directory_uri() . '/assets/css/bootstrap.css');
