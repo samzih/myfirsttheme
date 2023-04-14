961465ae9675bd50004426e95938f4a86454404d
samzih <samzih1337@gmail.com>
2023-04-14 02:20:35 +0200

    added theme support for widgets

--- functions.php
+++ functions.php
@@ -4,6 +4,7 @@
 function theme_support(){
     add_theme_support('menus');
     add_theme_support('post-thumbnails');
+    add_theme_support('widgets');
 }
 add_action('after_setup_theme', 'theme_support');
 
