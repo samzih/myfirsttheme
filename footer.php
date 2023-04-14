b108841ec16f20adde29c96f449fd059bdc0f94f
samzih <samzih1337@gmail.com>
2023-04-14 20:42:24 +0200

    minor code placement adjustments

--- footer.php
+++ footer.php
@@ -8,32 +8,18 @@
                 <?php dynamic_sidebar('footer-widget-area-2'); ?>
             </div>
             <div class="col-xs-12 col-sm-3 col-md-3 col-md-offset-1">
-                <h4>Social media</h4>
-                <ul class="social">
-                    <li>
-                        <i class="fa fa-facebook"></i> <a href="">Facebook</a>
-                    </li>
-                    <li>
-                        <i class="fa fa-twitter"></i> <a href="">Twitter</a>
-                    </li>
-                    <li>
-                        <i class="fa fa-instagram"></i> <a href="">Instagram</a>
-                    </li>
-                    <li>
-                        <i class="fa fa-linkedin"></i> <a href="">LinkedIn</a>
-                    </li>
-                </ul>
+                <?php dynamic_sidebar('footer-widget-area-3'); ?>
             </div>
         </div>
         <div class="row bottom">
             <div class="col-xs-12">
-                <?php dynamic_sidebar('footer-widget-area-3'); ?>
+                <?php dynamic_sidebar('footer-widget-area-4'); ?>
             </div>
         </div>
     </div>
-    <?php wp_footer(); // injects various content (scripts, stylesheets & more) into the footer section
-    ?>
 </footer>
+
+<?php wp_footer(); ?> <!-- injects various content (scripts, stylesheets & more) into the footer section -->
 </body>
 
 </html>
