c3f0f03dc2c27ccb15dd06d5da3e704b6226cf7b
samzih <samzih1337@gmail.com>
2023-04-15 15:43:05 +0200

    now uses template parts for better readability

--- home.php
+++ home.php
@@ -5,39 +5,24 @@
         <div class="container">
             <div class="row">
                 <div id="primary" class="col-xs-12 col-md-9">
-                    <h1><?php wp_title(''); ?></h1>
-                    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-                            <article>
-                                <img src="<?php the_post_thumbnail_url(); ?>">
-                                <h2 class="title">
-                                    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-                                </h2>
-                                <ul class="meta">
-                                    <li>
-                                        <i class="fa fa-calendar"></i> <?php echo get_the_date('j F, Y'); ?>
-                                    </li>
-                                    <li>
-                                        <i class="fa fa-user"></i> <a href="<?php the_permalink(); ?>"><?php the_author_posts_link(); ?></a>
-                                    </li>
-                                    <li>
-                                        <i class="fa fa-tag"></i> <?php the_category(', ') ?>
-                                    </li>
-                                </ul>
-                                <p><?php the_excerpt(); ?></p>
-                            </article>
-                    <?php endwhile;
+
+                    <h1><?php single_post_title(); ?></h1>
+
+                    <?php
+                    if (have_posts()) :
+                        while (have_posts()) :
+                            the_post();
+                            get_template_part('modules/post', 'snippet');
+                        endwhile;
                     endif;
+
+                    get_template_part('modules/pagination');
                     ?>
-                    <nav class="navigation pagination">
-                        <h2 class="screen-reader-text">Inläggsnavigering</h2>
-                        <?php echo paginate_links(
-                            array(
-                                'prev_text' => 'Föregående',
-                                'next_text' => 'Nästa',
-                            )
-                        ); ?>
-                    </nav>
+
                 </div>
+
+                <?php get_sidebar(); ?>
+
             </div>
         </div>
     </section>
