b618b88e45d839b61ff8ec93db82c406e44eb376
samzih <samzih1337@gmail.com>
2023-04-15 02:04:18 +0200

    undersida aside menu template part

---
+++ modules/undersida-aside.php
@@ -0,0 +1,7 @@
+<ul class="side-menu">
+    <?php wp_nav_menu(array(
+        'theme_location' => 'side-menu',
+        'menu_class' => 'side-menu',
+        'container' => 'true',
+    )); ?>
+</ul>
