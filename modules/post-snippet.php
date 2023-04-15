b23a4dc1f87e7969a0f268d1f8866c45f1aa1186
samzih <samzih1337@gmail.com>
2023-04-15 15:12:17 +0200

    post metadata + excerpt template part

---
+++ modules/post-snippet.php
@@ -0,0 +1,8 @@
+<article>
+    <img src="<?php the_post_thumbnail_url(); ?>" />
+    <h2 class="title">
+        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+    </h2>
+    <?php get_template_part('modules/post', 'metadata'); ?>
+    <p><?php the_excerpt(); ?></p>
+</article>
