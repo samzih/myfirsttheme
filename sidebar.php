3372b8f0fa5d7f47aedfc38e4a25cc7927eb44f9
samzih <samzih1337@gmail.com>
2023-04-15 15:15:44 +0200

    sidebar is now dynamic (widgets)

--- sidebar.php
+++ sidebar.php
@@ -1,67 +1,20 @@
-<div id="sidebar">
-    <ul>
-        <li>
-            <form id="searchform" class="searchform">
-                <div>
-                    <label class="screen-reader-text">Sök efter:</label>
-                    <input type="text" />
-                    <input type="submit" value="Sök" />
-                </div>
-            </form>
-        </li>
-    </ul>
-    <ul role="navigation">
-        <li class="pagenav">
-            <h2>Sidor</h2>
-            <ul>
-                <li class="page_item current_page_item">
-                    <a href="">Blogg</a>
-                </li>
-                <li class="page_item">
-                    <a href="">Exempelsida</a>
-                </li>
-                <li class="page_item">
-                    <a href="">Kontakt</a>
-                </li>
-                <li class="page_item page_item_has_children">
-                    <a href="">Om mig</a>
-                    <ul class="children">
-                        <li class="page_item">
-                            <a href="">Intressen</a>
-                        </li>
-                        <li class="page_item page_item_has_children">
-                            <a href="">Portfolio</a>
-                            <ul class="children">
-                                <li class="page_item">
-                                    <a href="">Projekt 1</a>
-                                </li>
-                            </ul>
-                        </li>
-                    </ul>
-                </li>
-                <li class="page_item">
-                    <a href="">Startsida</a>
-                </li>
-            </ul>
-        </li>
-        <li>
-            <h2>Arkiv</h2>
-            <ul>
-                <li>
-                    <a href="arkiv.html">oktober 2016</a>
-                </li>
-            </ul>
-        </li>
-        <li class="categories">
-            <h2>Kategorier</h2>
-            <ul>
-                <li class="cat-item">
-                    <a href="">Natur</a> (1)
-                </li>
-                <li class="cat-item">
-                    <a href="">Okategoriserade</a> (3)
-                </li>
-            </ul>
-        </li>
-    </ul>
-</div>
+<aside id="secondary" class="col-xs-12 col-md-3">
+    <div id="sidebar">
+        <ul>
+            <li>
+                <?php dynamic_sidebar('sidebar-widget-area-1'); ?>
+            </li>
+        </ul>
+        <ul role="navigation">
+            <li class="pagenav">
+                <?php dynamic_sidebar('sidebar-widget-area-2'); ?>
+            </li>
+            <li>
+                <?php dynamic_sidebar('sidebar-widget-area-3'); ?>
+            </li>
+            <li class="categories">
+                <?php dynamic_sidebar('sidebar-widget-area-4'); ?>
+            </li>
+        </ul>
+    </div>
+</aside>
