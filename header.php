e0a6389a34179954b2304086c0bc9ddb9bf15aff
samzih <samzih1337@gmail.com>
2023-04-15 16:23:23 +0200

    removed some unnecessary HTML

--- header.php
+++ header.php
@@ -11,41 +11,34 @@
 
 <body>
     <div id="wrap">
-
         <header id="header">
             <div class="container">
                 <div class="row">
+
                     <div class="col-xs-8 col-sm-6">
                         <a class="logo" href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a>
                     </div>
+
                     <div class="col-sm-6 hidden-xs">
                         <form id="searchform" class="searchform">
-                            <div>
-                                <?php get_search_form(); ?>
-                                <!-- <label class="screen-reader-text">Sök efter:</label>
-                                <input type="text" />
-                                <input type="submit" value="Sök" /> -->
-                            </div>
+                            <?php get_search_form(); ?>
                         </form>
                     </div>
+
                     <div class="col-xs-4 text-right visible-xs">
                         <div class="mobile-menu-wrap">
                             <i class="fa fa-search"></i>
                             <i class="fa fa-bars menu-icon"></i>
                         </div>
                     </div>
+
                 </div>
             </div>
         </header>
 
         <div class="mobile-search">
             <form id="searchform" class="searchform">
-                <div>
-                    <?php get_search_form(); ?>
-                    <!-- <label class="screen-reader-text">Sök efter:</label>
-                    <input type="text" />
-                    <input type="submit" value="Sök" /> -->
-                </div>
+                <?php get_search_form(); ?>
             </form>
         </div>
 
