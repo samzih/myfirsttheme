ebfd108ba7296c13a978511cc58e09c97dd1dd5e
samzih <samzih1337@gmail.com>
2023-04-15 15:31:46 +0200

    archive displays posts correctly

--- archive.php
+++ archive.php
@@ -1,1 +1,31 @@
-this is the archive.php file
+<?php get_header(); ?>
+
+<main>
+    <section>
+        <div class="container">
+            <div class="row">
+                <div id="primary" class="col-xs-12 col-md-9">
+
+                    <h1>Arkiv: <?php wp_title(''); ?></h1>
+
+                    <?php
+                    if (have_posts()) :
+                        while (have_posts()) :
+                            the_post();
+                            get_template_part('modules/post', 'snippet');
+                        endwhile;
+                    endif;
+
+                    get_template_part('modules/pagination');
+                    ?>
+
+                </div>
+
+                <?php get_sidebar(); ?>
+
+            </div>
+        </div>
+    </section>
+</main>
+
+<?php get_footer(); ?>
